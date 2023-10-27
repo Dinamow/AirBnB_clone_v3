02d484879530750180b615252fb441e0df492ae6
Ahmed Mohamed <ahmedmoh0107@gmail.com>
2023-10-27 13:46:51 +0000

    finish 4. Some stats?

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,14 +3,29 @@
 from api.v1.views import app_views
 from flask import jsonify
 from models import storage
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
+
 
 @app_views.route('/status', strict_slashes=False)
 def statusok():
     """status home page"""
     return jsonify(status='OK'), 200
 
+
 @app_views.route('/stats', strict_slashes=False)
 def statssok():
     """stats home page"""
-    data = {"amenities" : storage.count()}
-    return jsonify(data), 200
+    data = {
+            "amenities": storage.count(Amenity),
+            "cities": storage.count(City),
+            "places": storage.count(Place),
+            "reviews": storage.count(Review),
+            "states": storage.count(State),
+            "users": storage.count(User),
+           }
+    return data
