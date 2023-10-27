0bed42ba8b8454f7e9fdcf735e71b67b923f33d5
abdelrhman mohammed <meemoo102039@gmail.com>
2023-10-27 12:36:56 +0000

    to be con

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,8 +2,15 @@
 """the index file"""
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
 
 @app_views.route('/status', strict_slashes=False)
 def statusok():
     """status home page"""
     return jsonify(status='OK'), 200
+
+@app_views.route('/stats', strict_slashes=False)
+def statssok():
+    """stats home page"""
+    data = {"amenities" : storage.count()}
+    return jsonify(data), 200
