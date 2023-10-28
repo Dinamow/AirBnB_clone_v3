92fb6e723cfe8f300f3b7f9cd5f4d1bc153ddc83
Ahmed Mohamed <ahmedmoh0107@gmail.com>
2023-10-28 19:28:50 +0000

    Add amenities.py

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -5,7 +5,6 @@ from api.v1.views import app_views
 from flask import jsonify, abort, request
 from models import storage
 from models.state import State
-from models.city import City
 
 
 @app_views.route('/states', methods=['GET'], strict_slashes=False)
