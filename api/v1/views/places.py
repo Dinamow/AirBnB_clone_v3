ab06597118c7e3c5495a913f20c0d719bbd5ca43
abdelrhman mohammed <meemoo102039@gmail.com>
2023-10-29 15:20:48 +0000

    fix merge

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -3,23 +3,6 @@
 from api.v1.views import app_views
 from flask import jsonify, abort, request
 from models import storage
-<<<<<<< HEAD
-from models.city import City
-from models.place import Place
-
-
-@app_views.route('/cities/<string:city_id>/places', methods=['GET'], strict_slashes=False)
-def listofplaces(city_id):
-    """comment for func"""
-    ob = storage.all('Place')
-    ll = []
-    for place in ob.values():
-        if place.to_dict()['city_id'] == city_id:
-            ll.append(place)
-    if len(ll) <= 0:
-        abort(404)
-    return jsonify(ll), 200
-=======
 from models.place import Place
 from models.city import City
 
@@ -33,4 +16,3 @@ def getPlacesByCity(city_id):
     for place in ob.values():
         ll.append(place.to_dict())
     return jsonify(ll)
->>>>>>> 7714ad96c8dff0e17f7d8daa503251e464005968
