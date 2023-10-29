561cf7f01a3ccaf7810a203e5bf46bc63b4f4e68
abdelrhman mohammed <meemoo102039@gmail.com>
2023-10-29 14:38:07 +0000

    places get req

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -4,6 +4,18 @@
 from api.v1.views import app_views
 from flask import jsonify, abort, request
 from models import storage
-from models.state import State
+from models.city import City
+from models.place import Place
 
-pass
+
+@app_views.route('/cities/<string:city_id>/places', methods=['GET'], strict_slashes=False)
+def listofplaces(city_id):
+    """comment for func"""
+    ob = storage.all('Place')
+    ll = []
+    for place in ob.values():
+        if place.to_dict()['city_id'] == city_id:
+            ll.append(place)
+    if len(ll) <= 0:
+        abort(404)
+    return jsonify(ll), 200
